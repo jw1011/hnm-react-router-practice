d2e164869017db1d334cb77ec03a52cd90a70ad6
Juwon Lee <a8316010@gmail.com>
2023-04-20 15:36:15 +0900

    updated product detail page

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Dropdown } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return <div>상품 디테일 페이지</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="product-img">
+            <img src={product?.img} alt="" />
+          </Col>
+          <Col>
+            <div>{product?.title}</div>
+            <div>{product?.price}</div>
+            <div>Conscious choice</div>
+            <div>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  사이즈 선택
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">S</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">M</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">L</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div>
+              <button className="add-button">추가</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductDetail;
