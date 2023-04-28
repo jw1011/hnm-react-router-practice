67c5b305b8b5998d4f0a429044447857b2b45643
Juwon Lee <wndnjs6010@naver.com>
2023-04-28 16:21:28 +0900

    updated productActions

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,10 +1,13 @@
+import { productActions } from "../reducers/productReducer";
+
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
     let url = `https://my-json-server.typicode.com/jw1011
     /hnm-react-router-practice/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
-    dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    // dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
+    dispatch(productActions.getAllProducts({ data }));
   };
 }
 
@@ -14,7 +17,8 @@ function getProductDetail(id) {
     /hnm-react-router-practice/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
-    dispatch({ type: "GET_SINGLE_PRODUCT_SUCCESS", payload: { data } });
+    // dispatch({ type: "GET_SINGLE_PRODUCT_SUCCESS", payload: { data } });
+    dispatch(productActions.getSingleProduct({ data }));
   };
 }
 export const productAction = { getProducts, getProductDetail };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,23 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
 
-let store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+import productReducer from "./reducers/productReducer";
+import authenticateReducer from "./reducers/authenticateReducer";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// let store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: {
+    auth: authenticateReducer,
+    product: productReducer,
+  },
+});
 
 export default store;
